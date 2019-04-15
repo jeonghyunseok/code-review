61e35e8f13083b3fca1573e495c58b0dee509ae3
igloosiem <igloosiem@igloosec.com>
2019-04-15 14:19:52 +0900

    push test

--- projects/App.jsx
+++ projects/App.jsx
@@ -1,17 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import './App.css'
+
+
+const Links = () => {
+    <nav>
+        <Link to="/?id=123">Inline</Link>   
+        <Link to={{pathname:'/', search:'id=456&x=1'}}>Object</Link>  
+    </nav>
+}
 const App = () => (
-    <Router>
-        <div>
-            <Links />
-            <Route
-                path="/:date(\) : ext(\)">
-                render={({ match }) => (
-                    <div>
-                        <h1> YES Matched: </h1>
-                        <div> date : {match.params.date}</div>
-                        <div> ext : {match.params.ext}</div>
-                    </div>
-                )}
-            </Route>
-        </div>
-    </Router>
+<Router>
+<div>
+    <Links />
+    <Route
+        path="/">
+        render={({ match ,location}) => (
+            <div>
+                <h1> root </h1>
+                <div>{JSON.stringify(match)}</div>
+                <div>{JSON.stringify(location)}</div>
+            </div>
+        )}
+    </Route>
+</div>
+</Router>
 )
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
