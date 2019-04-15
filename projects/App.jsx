57d45c0546187fd06a03ea68939e839250463c85
igloosiem <igloosiem@igloosec.com>
2019-04-15 15:03:44 +0900

    push

--- projects/App.jsx
+++ projects/App.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import './App.css'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import './App.css';
 
 
-const Links = () => {
+const Links = () => (
     <nav>
-        <Link to="/?id=123">Inline</Link>   
-        <Link to={{pathname:'/', search:'id=456&x=1'}}>Object</Link>  
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
     </nav>
-}
+)
+
+const Home = () => <h1>Home</h1>;
+const Menu = () => (
+    <div>
+        <h1>Menu</h1>
+        <nav>
+            <Link to="../menu/food">Food</Link>
+            <Link to="../menu/drinks">Drinks</Link>
+            <Link to="../menu/sides">Sides</Link>
+        </nav>
+    </div>
+)
 const App = () => (
-<Router>
-<div>
-    <Links />
-    <Route
-        path="/">
-        render={({ match ,location}) => (
-            <div>
-                <h1> root </h1>
-                <div>{JSON.stringify(match)}</div>
-                <div>{JSON.stringify(location)}</div>
-            </div>
-        )}
-    </Route>
-</div>
-</Router>
+    <Router>
+        <Links />
+        <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/menu" component={Menu} />
+        </Switch>
+    </Router>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'));
